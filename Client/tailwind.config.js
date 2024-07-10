3a28746ef1b29af5a05826d1d42113ee2f2ce62e
Sambashivarao-Boyina <boyinasambashivarao@gmail.com>
2024-07-10 22:48:10 +0530

    added

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -2,6 +2,7 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 export default withMT({
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
