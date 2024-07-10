ee1d95d5416563af649d0f75bb725b3ea9877527
Sambashivarao-Boyina <boyinasambashivarao@gmail.com>
2024-07-10 23:06:49 +0530

    added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -54,7 +54,7 @@ function App() {
     },[])
 
     return (
-        <div className='bg-white'>
+        <>
             <ComplexNavbar/>
             <Routes >
                 <Route  path="/" element={<Products/>}/>
@@ -80,7 +80,7 @@ function App() {
 
                 <Route path="*" element={<PageNotFound/>}/>
             </Routes>       
-        </div>
+        </>
     )
 }
 
